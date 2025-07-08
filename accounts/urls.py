77d56c48ceb9785d8d12702670cef4133e16bb5e
its-steo <steomustadd@gmail.com>
2025-07-08 18:04:38 +0300

    add changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('home/', views.home, name='home'),
+    path('accounts/login/', accounts_views.login_view, name='account_login'),
     
 ]
 
